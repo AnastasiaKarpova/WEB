2226cb32b9e59a475e2b0010e70e5ba8bfd87af8
Anastasia <karpovanastiya@mail.ru>
2025-09-17 22:49:42 +0300

    TODOList in progress

---
+++ Blazor/IntroductionToBlazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroductionToBlazor.Components.Pages
+{
+	public class Factorial
+	{
+		int n = 0;
+		long factorial = 1;
+		void Calculate()
+		{
+			factorial = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				factorial *= i;
+			}
+		}
+	}
+}

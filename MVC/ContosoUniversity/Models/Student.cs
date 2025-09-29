c0d181f27957c6db6023586a4fc04f35ec114a97
Anastasia <karpovanastiya@mail.ru>
2025-09-30 00:19:49 +0300

    UniversityContext registration DONE

---
+++ MVC/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        [Required]
+        public DateTime EnrollmentDate { get; set; } //Дата поступления
+        
+        //Navigation property
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}

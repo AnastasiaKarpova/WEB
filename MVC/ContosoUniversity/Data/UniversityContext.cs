25507ecfaae6066a1fcb8a1d5f966ef17e19102f
Anastasia <karpovanastiya@mail.ru>
2025-10-06 21:18:33 +0300

    UniversityContext registration DONE

--- MVC/ContosoUniversity/Data/UniversityContext.cs
+++ MVC/ContosoUniversity/Data/UniversityContext.cs
@@ -1,6 +1,6 @@
 ﻿using ContosoUniversity.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Data.Entity;
+//using System.Data.Entity;
 
 namespace ContosoUniversity.Data
 {
@@ -11,7 +11,7 @@ namespace ContosoUniversity.Data
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Student>().ToTable("Students");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollments");
